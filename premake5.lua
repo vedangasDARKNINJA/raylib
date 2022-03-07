02fb5e049700c8249c754f7c3476bd024b2126a4
vedangasDARKNINJA <vedang.javdekar@gmail.com>
2022-03-07 23:17:34 +0530

    Minimal Setup

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+project "raylib"
+    filter "configurations:Debug.DLL OR Release.DLL"
+        kind "SharedLib"
+        defines {"BUILD_LIBTYPE_SHARED"}
+        
+    filter "configurations:Debug OR Release"
+        kind "StaticLib"
+        
+    filter "action:vs*"
+        defines{"_WINSOCK_DEPRECATED_NO_WARNINGS", "_CRT_SECURE_NO_WARNINGS"}
+        characterset ("MBCS")
+    
+    filter "system:windows"
+        defines{"_WIN32"}
+        links {"winmm", "kernel32", "opengl32", "kernel32", "gdi32"}
+        
+    filter "system:linux"
+        links {"pthread", "GL", "m", "dl", "rt", "X11"}
+        
+    filter{}
+    
+    defines{
+		"PLATFORM_DESKTOP",
+		"GRAPHICS_API_OPENGL_43"
+	}
+    
+    language "C"
+    location "%{_MAIN_SCRIPT_DIR}/build/%{prj.name}"
+
+    targetdir "%{_MAIN_SCRIPT_DIR}/build/Game/bin/%{cfg.buildcfg}"
+    objdir "%{_MAIN_SCRIPT_DIR}/build/Game/bin-int/%{cfg.buildcfg}"
+    debugdir "%{_MAIN_SCRIPT_DIR}/"
+    
+    includedirs { "src", "src/external/glfw/include" }
+    vpaths 
+    {
+        ["Header Files"] = { "src/**.h"},
+        ["Source Files/*"] = {"src/**.c"},
+    }
+    files {"src/*.h", "src/*.c"}
